c98defaf657564bad258cbe984f20b6f0020e363
Sanjay Singh <sanjuofficial794@gmail.com>
2025-12-01 23:56:46 +0530

    fixed the add trip

--- src/api/tripApi.js
+++ src/api/tripApi.js
@@ -4,4 +4,4 @@ export const getAllTrips    = () => http.get("/api/admin/trips");
 export const deleteTrip     = (id) => http.delete(`/api/admin/trips/${id}`);
 
 // 📌 Create/Add a new trip
-export const addTrip = (tripData) => axios.post("/api/admin/trips", tripData);
+export const addTrip = (tripData) => http.post("/api/admin/trips", tripData);
